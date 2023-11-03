cc9e552ab69214ae0a35203ed8d3d2bba24c98f2
Pandours <lefevreflorian18@gmail.com>
2023-11-03 14:17:36 +0100

    [ADD] Basic clock

---
+++ Assets/Game/Scripts/Managers/Clock.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.IsartDigital.Rush
+{
+    public class Clock : MonoBehaviour
+    {
+        #region Singleton
+        private static Clock _Instance = null; 
+
+        private Clock() : base() { }
+
+        public static Clock GetInstance()
+        {
+            if (_Instance == null)
+                _Instance = new Clock();
+
+            return _Instance;
+        }
+
+        #endregion
+
+        private const float DURATION_BETWEEN_TICK = 1f;
+
+        private const float MAX_MULTIPLIER = 5f;
+        private const float MIN_MULTIPLIER = .1f;
+
+        [SerializeField][Range(MIN_MULTIPLIER, MAX_MULTIPLIER)] private float _TickMultiplier = 1f;
+
+        private Coroutine _InternalTimer = null;
+
+        private float _UpdatedDurationTick = DURATION_BETWEEN_TICK;
+        public float ElapsedTime { get; private set; } = 0f;
+
+        // Signals
+        public event Action OnTick;
+
+        private void Awake()
+        {
+            if (_Instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            _Instance = this;
+        }
+        
+        private void Start()
+        {
+            _InternalTimer = StartCoroutine(Tick());
+        }
+
+        private void Update() => ElapsedTime += Time.deltaTime * _TickMultiplier;
+
+        private IEnumerator Tick()
+        {
+            while (isActiveAndEnabled)
+            {
+                yield return new WaitForSeconds(_UpdatedDurationTick);
+                OnTick?.Invoke();
+
+                ElapsedTime -= _UpdatedDurationTick;
+            }
+
+            StopCoroutine(_InternalTimer);
+            yield return null;
+        }
+
+        public void UpdateTickMultiplier(int pMultiplier)
+        {
+            if (pMultiplier > MIN_MULTIPLIER || pMultiplier < MAX_MULTIPLIER)
+            {
+                _TickMultiplier = pMultiplier;
+                _UpdatedDurationTick = DURATION_BETWEEN_TICK * _TickMultiplier;
+            } 
+        }
+
+        private void OnDestroy()
+        {
+            if (_InternalTimer != null)
+                StopCoroutine(Tick());
+
+            if (_Instance != null)
+                _Instance = null;
+        }
+    }
+}
