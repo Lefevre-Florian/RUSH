72915bbb0f3c4b5e0b76a8238b0b230f59a7554f
Lefevre-Florian <lefevreflorian18@gmail.com>
2023-11-29 22:49:04 +0100

    [ADD] Glyphs in teleporter + option to target camera position for world space UI

--- Assets/Game/Scripts/Accessibility/TileAccessibilityRenderer.cs
+++ Assets/Game/Scripts/Accessibility/TileAccessibilityRenderer.cs
@@ -1,3 +1,4 @@
+using Com.IsartDigital.Rush.Camera;
 using Com.IsartDigital.Rush.Tiles;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,6 +9,11 @@ namespace Com.IsartDigital.Rush.Accessibility
     [RequireComponent(typeof(ColoredTiles))]
     public class TileAccessibilityRenderer : AccessibilityRenderer
     {
+        [SerializeField] private bool _IsStatic = true;
+
+        private OrbitalCamera _Camera = null;
+        private Transform _Icon = null;
+
         private void Start()
         {
             Colors lID = GetComponent<ColoredTiles>().Color;
@@ -17,9 +23,37 @@ namespace Com.IsartDigital.Rush.Accessibility
             {
                 Transform lIcon = Instantiate(m_Prefab,
                                               transform.position + Vector3.up * m_Height,
-                                              new Quaternion(),
+                                              m_Prefab.transform.rotation,
                                               transform).transform;
                 lIcon.GetComponentInChildren<Image>().sprite = lTexture;
+
+                if (_IsStatic)
+                    lIcon.gameObject.isStatic = true;
+                else
+                {
+                    _Icon = lIcon;
+
+                    _Camera = OrbitalCamera.GetInstance();
+                    _Camera.OnMove += LookAt;
+                }
+            }
+        }
+
+        private void LookAt()
+        {
+            _Icon.LookAt(new Vector3(_Camera.transform.position.x,
+                                     _Icon.position.y,
+                                     _Camera.transform.position.z));
+        }
+
+        private void OnDestroy()
+        {
+            _Icon = null;
+
+            if (!_IsStatic)
+            {
+                _Camera.OnMove -= LookAt;
+                _Camera = null;
             }
         }
     }
